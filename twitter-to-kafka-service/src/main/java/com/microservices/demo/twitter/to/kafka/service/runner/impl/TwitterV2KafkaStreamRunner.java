47f36ff8e39fdd1d07a0d1faf0928637882816b6
Johan Saputro Budiono <johanbudiono@Johan-Budiono.local>
2022-07-01 14:38:31 +0700

    add Twitter V2 API for streaming data instead of mock

--- twitter-to-kafka-service/src/main/java/com/microservices/demo/twitter/to/kafka/service/runner/impl/TwitterV2KafkaStreamRunner.java
+++ twitter-to-kafka-service/src/main/java/com/microservices/demo/twitter/to/kafka/service/runner/impl/TwitterV2KafkaStreamRunner.java
@@ -1,57 +1,61 @@
 package com.microservices.demo.twitter.to.kafka.service.runner.impl;
 
 import com.microservices.demo.config.TwitterV2ToKafkaServiceConfigData;
-import com.microservices.demo.twitter.to.kafka.service.listener.TwitterKafkaStatusListener;
 import com.microservices.demo.twitter.to.kafka.service.runner.StreamRunner;
-import java.util.Arrays;
-import javax.annotation.PreDestroy;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import org.json.JSONException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
 import org.springframework.stereotype.Component;
-import twitter4j.FilterQuery;
 import twitter4j.TwitterException;
-import twitter4j.TwitterStream;
-import twitter4j.TwitterStreamFactory;
 
 @Component
 @ConditionalOnExpression("${twitter-to-kafka-service-v2.enable-v2-tweets} && not ${twitter-to-kafka-service-v2.enable-mock-tweets}")
 public class TwitterV2KafkaStreamRunner implements StreamRunner {
 
-  private static final Logger LOG = LoggerFactory.getLogger(TwitterKafkaStreamRunner.class);
+  private static final Logger LOG = LoggerFactory.getLogger(TwitterV2KafkaStreamRunner.class);
 
   private final TwitterV2ToKafkaServiceConfigData twitterToKafkaServiceConfigData;
 
-  private final TwitterKafkaStatusListener twitterKafkaStatusListener;
-
-  private TwitterStream twitterStream;
+  private final TwitterV2StreamHelper twitterV2StreamHelper;
 
   public TwitterV2KafkaStreamRunner(
-      TwitterV2ToKafkaServiceConfigData twitterToKafkaServiceConfigData,
-      TwitterKafkaStatusListener twitterKafkaStatusListener) {
+      TwitterV2ToKafkaServiceConfigData twitterToKafkaServiceConfigData, TwitterV2StreamHelper twitterV2StreamHelper) {
     this.twitterToKafkaServiceConfigData = twitterToKafkaServiceConfigData;
-    this.twitterKafkaStatusListener = twitterKafkaStatusListener;
+    this.twitterV2StreamHelper = twitterV2StreamHelper;
   }
 
   @Override
   public void start() throws TwitterException {
-    twitterStream = new TwitterStreamFactory().getInstance();
-    twitterStream.addListener(twitterKafkaStatusListener);
-    addFilter();
-  }
-
-  @PreDestroy
-  public void shutdown() {
-    if (twitterStream != null) {
-      LOG.info("Closing twitter stream!");
-      twitterStream.shutdown();
+    String bearerToken = twitterToKafkaServiceConfigData.getTwitterV2BearerToken();
+    if (null != bearerToken) {
+      try {
+        twitterV2StreamHelper.setupRules(bearerToken, getRules());
+        twitterV2StreamHelper.connectStream(bearerToken);
+      } catch (IOException | URISyntaxException | JSONException e) {
+        LOG.error("Error streaming tweets!", e);
+        throw new RuntimeException("Error streaming tweets!", e);
+      }
+    } else {
+      LOG.error("There was a problem getting your bearer token. " +
+          "Please make sure you set the TWITTER_BEARER_TOKEN environment variable");
+      throw new RuntimeException("There was a problem getting your bearer token. +" +
+          "Please make sure you set the TWITTER_BEARER_TOKEN environment variable");
     }
   }
 
-  private void addFilter() {
-    String[] keywords = twitterToKafkaServiceConfigData.getTwitterKeywords().toArray(new String[0]);
-    FilterQuery filterQuery = new FilterQuery(keywords);
-    twitterStream.filter(filterQuery);
-    LOG.info("Started filtering twitter stream for keywords {}", Arrays.toString(keywords));
+  private Map<String, String> getRules() {
+    List<String> keywords = twitterToKafkaServiceConfigData.getTwitterKeywords();
+    Map<String, String> rules = new HashMap<>();
+    for (String keyword: keywords) {
+      rules.put(keyword, "Keyword: " + keyword);
+    }
+    LOG.info("Created filter for twitter stream for keywords: {}", keywords);
+    return rules;
   }
 }
